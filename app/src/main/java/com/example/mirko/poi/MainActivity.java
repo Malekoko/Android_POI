722d44ee63be041db11d9e523c483d869b1f70c0
peacook94 <RainerZ01@googlemail.com>
2016-01-11 18:08:46 +0100

    Testcommit nochmal!

--- app/src/main/java/com/example/mirko/poi/MainActivity.java
+++ app/src/main/java/com/example/mirko/poi/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      *
-     * Hier kommt ein Testkommentar von REINHARD!!
+     * Hier kommt ein Testkommentar von REINHARD!!cvbcbvcvbcnbcnbvcvbcbvcbvcbvcbvc
      */
     private GoogleApiClient client;
 
